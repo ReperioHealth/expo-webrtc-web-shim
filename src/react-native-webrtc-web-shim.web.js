692b55582eab6817e68f80066af99758798bd28d
ericlyoung <118756309+ericlyoung@users.noreply.github.com>
2024-03-18 11:22:18 -0700

    Update react-native-webrtc-web-shim.web.js

--- src/react-native-webrtc-web-shim.web.js
+++ src/react-native-webrtc-web-shim.web.js
@@ -1,13 +1,25 @@
-import RTCView from './RTCView';
+import RTCView from './RTCView'
 
-if (typeof window !== "undefined") {
-  window.MediaStream.prototype.release = function release() {
-    this.getTracks().forEach((track) => track.stop());
-  };
-  
-  window.MediaStreamTrack.prototype._switchCamera = function _switchCamera() {
-    console.warn('_switchCamera is not implemented on web.');
-  };
+if (typeof window !== 'undefined') {
+  window.MediaStream.prototype.release = function release () {
+    this.getTracks().forEach((track) => track.stop())
+  }
+
+  window.MediaStreamTrack.prototype._switchCamera = function _switchCamera () {
+    console.warn('_switchCamera is not implemented on web.')
+  }
+}
+
+const window = window || {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCRtpTransceiver,
+  RTCRtpReceiver,
+  RTCRtpSender,
+  RTCErrorEvent,
+  MediaStream,
+  MediaStreamTrack
 }
 
 const {
@@ -20,24 +32,16 @@ const {
   RTCErrorEvent,
   MediaStream,
   MediaStreamTrack
-} = (window || {
-  RTCPeerConnection,
-  RTCIceCandidate,
-  RTCSessionDescription,
-  RTCRtpTransceiver,
-  RTCRtpReceiver,
-  RTCRtpSender,
-  RTCErrorEvent,
-  MediaStream,
-  MediaStreamTrack
-});
+} = window
 
-const { mediaDevices, permissions } = navigator;
+const navigator = navigator || { mediaDevices, permissions }
 
-function registerGlobals() {
-  if (typeof window !== "undefined") {
-    window.mediaDevices = navigator.mediaDevices;
-    window.permissions = navigator.permissions;
+const { mediaDevices, permissions } = navigator
+
+function registerGlobals () {
+  if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
+    window.mediaDevices = navigator.mediaDevices
+    window.permissions = navigator.permissions
   }
 }
 
@@ -54,5 +58,5 @@ export {
   mediaDevices,
   permissions,
   registerGlobals,
-  RTCView,
-};
+  RTCView
+}
